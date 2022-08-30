faf1acd3ccec6ddcdd52ca14ffe645bacb0764bb
adeboyeMary <maryadeboye69@gmail.com>
2022-08-31 00:12:14 +0100

    first assignment

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function multiply(a, b) {
+    return a*b
+}
+
+console.log(multiply(2, 3))
+
+function smartBot(myName) {
+  console.log(`My name is ${myName} and I am 35 years old`);
+}
+smartBot('Oma');
+
+// function numberSum(fir, sec) {
+//     console.log(`${fir} ${sec}`);
+// }
+// numberSum(10 + 4);
+
+// OR
+function numberSum(fir, sec) {
+    return fir + sec;
+}
+console.log(numberSum(10, 4));
